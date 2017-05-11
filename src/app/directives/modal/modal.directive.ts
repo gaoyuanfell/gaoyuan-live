e3fabc22711680d2b89962402a9c028c014a1f74
moka <gaoyuanfell@sina.com>
2017-05-11 21:50:31 +0800

    1

--- src/app/directives/modal/modal.directive.ts
+++ src/app/directives/modal/modal.directive.ts
@@ -1,35 +1,44 @@
-import {AfterViewInit, Directive, ElementRef, Input, Renderer2, ViewChild, ViewRef} from '@angular/core';
+import {AfterViewInit, Directive, ElementRef, Input, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[modal]'
 })
 export class ModalDirective implements AfterViewInit {
     ngAfterViewInit(): void {
-        let div = this.fadeDiv = document.createElement('div');
-        div.classList.add('modal-backdrop', 'fade', 'show');
-
-        this.renderer.listen(this._target, 'click', () => {
-            this._target.classList.remove('show');
-            this.fadeDiv.classList.remove('show');
-            setTimeout(() => {
-                this._target.style.display = 'none';
-                this.renderer.removeChild(document.body, this.fadeDiv)
-            }, this.time)
+        this.fadeDiv = document.createElement('div');
+        this.fadeDiv.classList.add('modal-backdrop', 'fade');
+        this.renderer.listen(this._target, 'click', (event:Event) => {
+            event.stopPropagation();
+            event.preventDefault();
+            if(this._target != event.target) return;
+            this.close();
         })
     }
 
     constructor(el: ElementRef, private renderer: Renderer2,) {
         renderer.listen(el.nativeElement, 'click', () => {
-            document.body.classList.add('modal-open');
-            this.fadeDiv.classList.add('show');
-            document.body.appendChild(this.fadeDiv);
-
-            this._target.style.display = 'block';
-
-            setTimeout(() => {
-                this._target.classList.add('show');
-            })
+            this.show();
         });
+    }
+
+    show(){
+        document.body.classList.add('modal-open');
+        document.body.appendChild(this.fadeDiv);
+        this._target.style.display = 'block';
+        setTimeout(() => {
+            this.fadeDiv.classList.add('show');
+            this._target.classList.add('show');
+        },100)
+    }
+
+    close(){
+        this._target.classList.remove('show');
+        this.fadeDiv.classList.remove('show');
+        document.body.classList.remove('modal-open');
+        setTimeout(() => {
+            this._target.style.display = 'none';
+            this.renderer.removeChild(document.body, this.fadeDiv)
+        }, this.time)
     }
 
     private time: number = 150;
