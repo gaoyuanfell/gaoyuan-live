4de83081a86bf3b6ca319748a2ce501c7685a9e4
moka <gaoyuanfell@sina.com>
2017-06-09 17:53:43 +0800

    1

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.less']
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.less']
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+    imgs:string[] = ['../../assets/img/1.jpg','../../assets/img/5.jpg','../../assets/img/6.jpg','../../assets/img/7.jpg']
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
