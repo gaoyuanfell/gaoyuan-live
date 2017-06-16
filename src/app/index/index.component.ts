bb59965325adb6897e1ab88c55a019a3f36eca97
moka <gaoyuanfell@sina.com>
2017-06-16 12:53:21 +0800

    gh

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-    imgs:string[] = ['../../assets/img/1.jpg','../../assets/img/5.jpg','../../assets/img/6.jpg','../../assets/img/7.jpg']
+    imgs:string[] = ['assets/img/1.jpg','assets/img/5.jpg','assets/img/6.jpg','assets/img/7.jpg']
 
     constructor() {
     }
